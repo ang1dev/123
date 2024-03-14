31f1cc8383758b3cf9947480a34b66a79a217f17
ang1dev <anevcheva@gmail.com>
2024-03-14 13:32:02 +0100

    implement a test

--- src/all.test.ts
+++ src/all.test.ts
@@ -1,6 +1,5 @@
 import * as amqp from "amqplib/callback_api";
-
-import { sendMessage } from "./amqfunc";
+import { sendMessage, receiveMessage } from "./amqfunc"; // Import receiveMessage function
 
 describe("RabbitMQ Tests", () => {
   let connection: amqp.Connection;
@@ -9,67 +8,46 @@ describe("RabbitMQ Tests", () => {
 
   beforeAll(() => {
     channelReady = new Promise((resolve, reject) => {
-       amqp.connect("amqp://localhost:5672", (error0: any, conn: amqp.Connection) => {
-         if (error0) {
+      amqp.connect("amqp://localhost:5672", (error0: any, conn: amqp.Connection) => {
+        if (error0) {
           console.error("Error connecting to RabbitMQ:", error0);
-           reject(error0);
-         }
-         connection = conn;
-         connection.createChannel((error1: any, ch: amqp.Channel) => {
-           if (error1) {
-             reject(error1);
-           }
-           channel = ch;
-           resolve(ch);
-         });
-       });
+          reject(error0);
+        }
+        connection = conn;
+        connection.createChannel((error1: any, ch: amqp.Channel) => {
+          if (error1) {
+            reject(error1);
+          }
+          channel = ch;
+          resolve(ch);
+        });
+      });
     });
-   });
-
-  afterAll((done) => {
-
-    connection.close();
-    done();
   });
 
-  // it("should send and receive message", (done) => {
-  //   const queue = "test_queue";
-  //   const msg = "Hello RabbitMQ!";
-
-  //   const consoleSpy = jest.spyOn(console, "log");
-
-  //   sendMessage(channel, queue, msg);
-
-
-  //   setTimeout(() => {
-  
-  //     receiveMessage(channel, queue);
-
-  //     setTimeout(() => {
-   
-  //       expect(consoleSpy).toHaveBeenCalledWith(`${msg}`);
-  //       done();
-  //     }, 1000); 
-  //   }, 1000); 
-  // });
+  afterAll((done) => {
+    connection.close(() => {
+      done();
+    });
+  });
 
   it("should send and receive message", async () => {
     const queue = "test_queue";
     const msg = "Hello RabbitMQ!";
-   
     const consoleSpy = jest.spyOn(console, "log");
-   
-    // Wait for the channel to be ready
-    const channel = await channelReady;
-   
+
+
+    await channelReady;
+
     sendMessage(channel, queue, msg);
 
-         setTimeout(() => {
-   
-        expect(consoleSpy).toHaveBeenCalledWith(`${msg}`);
-   
-      }, 1000); 
-    }, 1000); 
-   });
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
 
+    receiveMessage(channel, queue);
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    expect(consoleSpy).toHaveBeenCalledWith(`${msg}`);
+  });
+});
